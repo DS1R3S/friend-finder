1b1df33104a54bfd3cce92adeaed9d614c299345
Dan Sires <dsires13@mac.com>
2019-02-18 12:05:41 -0500

    5

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const PORT = process.env.PORT || 8080;
 // sets up 'Express' app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("./app/public"));
-app.get("/", function(req, res) {
-  res.json(path.join(__dirname, "/public/index.html"));
-});
+app.use(express.static("./app/public"));
+// app.get("/", function(req, res) {
+//   res.json(path.join(__dirname, "/public/index.html"));
+// });
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
